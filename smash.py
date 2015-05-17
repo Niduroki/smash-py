9c11029a12534ef0b1fe5ab4fadf4c8dd957848c
Christopher Schäpers <christopher@schaepers.it>
2015-05-17 02:32:46 +0200

    Add a real tierlist

--- smash.py
+++ smash.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from datetime import date
 import random
+import urllib.request
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -89,6 +91,84 @@ def amiibo_detail(shortcut, name):
 	reflex = random.choice(["exzellenten", "schnellen", "guten", "langsamen", "slow-motion"])
 	secret_weapon = random.choice(rating) + " " + random.choice(smash_attacks)
 	return render_template("amiibo_detail.html", char=character, name=name, ranged=ranged, melee=melee, random_attr=random_attr, reflex=reflex, secret_weapon=secret_weapon)
+
+@app.route('/real/')
+def real():
+	req = urllib.request.Request("http://www4.eventhubs.com/tiers/ssb4/")
+	html = urllib.request.urlopen(req).read().decode("utf-8")
+	soup = BeautifulSoup(html)
+	tier_list = []
+	for a in soup.find(id="tiers1").find_all("a"):
+		if "Vote" not in a:
+			if a.string == "Rosalina":
+				tier_list.append(search_for("Rosalina & Luma"))
+				continue
+			elif a.string == "Sheik":
+				tier_list.append(search_for("Shiek"))
+				continue
+			elif a.string == "Mewtwo":
+				tier_list.append(search_for("Mewtu"))
+				continue
+			elif a.string == "Villager":
+				tier_list.append(search_for("Bewohner"))
+				continue
+			elif a.string == "ROB":
+				tier_list.append(search_for("R.O.B."))
+				continue
+			elif a.string == "Robin":
+				tier_list.append(search_for("Daraen"))
+				continue
+			elif a.string == "Jigglypuff":
+				tier_list.append(search_for("Pummeluff"))
+				continue
+			elif a.string == "Greninja":
+				tier_list.append(search_for("Quajutsu"))
+				continue
+			elif a.string == "Wii Fit Trainer":
+				tier_list.append(search_for("Wii Fit-Trainerin"))
+				continue
+			elif a.string == "Dark Pit":
+				tier_list.append(search_for("Finsterer Pit"))
+				continue
+			elif a.string == "Toon Link":
+				tier_list.append(search_for("Toon-Link"))
+				continue
+			elif a.string == "King Dedede":
+				tier_list.append(search_for("König Dedede"))
+				continue
+			elif a.string == "Duckhunt":
+				tier_list.append(search_for("Duck-Hunt-Duo"))
+				continue
+			elif a.string == "Mr. Game and Watch":
+				tier_list.append(search_for("Mr. Game & Watch"))
+				continue
+			elif a.string == "Charizard":
+				tier_list.append(search_for("Glurak"))
+				continue
+			elif a.string == "Meta Knight":
+				tier_list.append(search_for("Meta-Knight"))
+			else:
+				for char in smash_chars:
+					if char[0] == a.string:
+						tier_list.append(char)
+						continue
+			print(a.string)
+
+	tier_list.append(search_for("Mii-Kämpfer"))
+
+	updated_at = str(date.today()) + " " + str(random.randint(3, 9)) + ":"
+	minute = random.randint(1,59)
+	if minute < 10:
+		minute = "0" + str(minute)
+	else:
+		minute = str(minute)
+	updated_at += minute
+	return render_template("index.html", smash_chars=tier_list, updated_at=updated_at)
+
+def search_for(name):
+	for char in smash_chars:
+		if char[0] == name:
+			return char
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True)
